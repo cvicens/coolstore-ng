26461af9e400e0b9ef5cf630c06a10481b515e24
cvicens <carlos.vicens.alonso@gmail.com>
2019-03-10 09:30:28 +0100

    working draft version

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,51 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
+// NGX Bootstrap
+import { BsDropdownConfig, BsDropdownModule } from 'ngx-bootstrap/dropdown';
+
+// Patternfly
+import { ApplicationLauncherModule } from 'patternfly-ng/navigation';
+import { VerticalNavigationModule } from 'patternfly-ng/navigation';
+
+// Components
 import { AppComponent } from './app.component';
+import { MainComponent } from './main/main.component';
+import { ProductsComponent } from './products/products.component';
+
+const appRoutes: Routes = [
+  { path: 'products', component: ProductsComponent },
+  // { path: 'hero/:id',      component: HeroDetailComponent },
+  // {
+  //   path: 'heroes',
+  //   component: HeroListComponent,
+  //   data: { title: 'Heroes List' }
+  // },
+  // { path: '',
+  //   redirectTo: '/heroes',
+  //   pathMatch: 'full'
+  // },
+  // { path: '**', component: PageNotFoundComponent }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainComponent,
+    ProductsComponent
   ],
   imports: [
+    ApplicationLauncherModule,
+    VerticalNavigationModule,
+    BsDropdownModule.forRoot(),
+    RouterModule.forRoot(
+      appRoutes,
+      { enableTracing: true } // <-- debugging purposes only
+    ),
     BrowserModule
   ],
-  providers: [],
+  providers: [BsDropdownConfig],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
